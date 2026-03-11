44a3762a6e517f3820d73a2c506661a7c45de908
Christa <christa_pol1@hotmail.com>
2026-03-11 13:01:05 +0100

    initial commit

--- src/routes/_student/vacancies/index.tsx
+++ src/routes/_student/vacancies/index.tsx
@@ -8,46 +8,46 @@ export const Route = createFileRoute("/_student/vacancies/")({
 
 function RouteComponent() {
   // dummy data to test
-  const data = {
-    items: [
-      {
-        id: 1,
-        company: "test",
-        title: "Test vacature 1",
-        hours_per_week: 40,
-        requirements: [
-          { id: 3, name: "Laravel" },
-          { id: 4, name: "SQL" },
-        ],
-        description:
-          "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec volutpat auctor eros. Praesent fermentum lectus non nibh maximus varius. Aliquam quis felis nec eros egestas viverra quis sed purus. Vestibulum placerat tortor at quam consectetur, ac mollis libero facilisis. Cras at est ac diam euismod ultricies. In eget ipsum ac est rhoncus tincidunt ut ut odio. Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
-        offer_text:
-          "Aliquam quis felis nec eros egestas viverra quis sed purus. Vestibulum placerat tortor at quam consectetur, ac mollis libero facilisis. ",
-        expectations_text:
-          "Cras at est ac diam euismod ultricies. In eget ipsum ac est rhoncus tincidunt ut ut odio. Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
-        matchscore: 50,
-        favorite: false,
-      },
-      {
-        id: 2,
-        company: "test2",
-        title: "Test vacature 2",
-        hours_per_week: 36,
-        requirements: [
-          { id: 1, name: "React.js" },
-          { id: 2, name: "Tailwind CSS" },
-        ],
-        description:
-          "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec volutpat auctor eros. Praesent fermentum lectus non nibh maximus varius. Aliquam quis felis nec eros egestas viverra quis sed purus. Vestibulum placerat tortor at quam consectetur, ac mollis libero facilisis. Cras at est ac diam euismod ultricies. In eget ipsum ac est rhoncus tincidunt ut ut odio. Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
-        offer_text:
-          "Aliquam quis felis nec eros egestas viverra quis sed purus. Vestibulum placerat tortor at quam consectetur, ac mollis libero facilisis. ",
-        expectations_text:
-          "Cras at est ac diam euismod ultricies. In eget ipsum ac est rhoncus tincidunt ut ut odio. Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
-        matchscore: 60,
-        favorite: true,
-      },
-    ],
-  };
+  // const data = {
+  //   items: [
+  //     {
+  //       id: 1,
+  //       company: "test",
+  //       title: "Test vacature 1",
+  //       hours_per_week: 40,
+  //       requirements: [
+  //         { id: 3, name: "Laravel" },
+  //         { id: 4, name: "SQL" },
+  //       ],
+  //       description:
+  //         "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec volutpat auctor eros. Praesent fermentum lectus non nibh maximus varius. Aliquam quis felis nec eros egestas viverra quis sed purus. Vestibulum placerat tortor at quam consectetur, ac mollis libero facilisis. Cras at est ac diam euismod ultricies. In eget ipsum ac est rhoncus tincidunt ut ut odio. Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+  //       offer_text:
+  //         "Aliquam quis felis nec eros egestas viverra quis sed purus. Vestibulum placerat tortor at quam consectetur, ac mollis libero facilisis. ",
+  //       expectations_text:
+  //         "Cras at est ac diam euismod ultricies. In eget ipsum ac est rhoncus tincidunt ut ut odio. Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+  //       matchscore: 50,
+  //       favorite: false,
+  //     },
+  //     {
+  //       id: 2,
+  //       company: "test2",
+  //       title: "Test vacature 2",
+  //       hours_per_week: 36,
+  //       requirements: [
+  //         { id: 1, name: "React.js" },
+  //         { id: 2, name: "Tailwind CSS" },
+  //       ],
+  //       description:
+  //         "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec volutpat auctor eros. Praesent fermentum lectus non nibh maximus varius. Aliquam quis felis nec eros egestas viverra quis sed purus. Vestibulum placerat tortor at quam consectetur, ac mollis libero facilisis. Cras at est ac diam euismod ultricies. In eget ipsum ac est rhoncus tincidunt ut ut odio. Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+  //       offer_text:
+  //         "Aliquam quis felis nec eros egestas viverra quis sed purus. Vestibulum placerat tortor at quam consectetur, ac mollis libero facilisis. ",
+  //       expectations_text:
+  //         "Cras at est ac diam euismod ultricies. In eget ipsum ac est rhoncus tincidunt ut ut odio. Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+  //       matchscore: 60,
+  //       favorite: true,
+  //     },
+  //   ],
+  // };
 
   // for if there is something in the backend:
   // const { data } = useQuery({
