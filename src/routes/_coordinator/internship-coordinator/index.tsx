08118ddbc65d4aedb73f6148611a8f5b8f804766
Christa <christa_pol1@hotmail.com>
2026-03-16 15:55:48 +0100

    Students overview is made,

--- src/routes/_coordinator/internship-coordinator/index.tsx
+++ src/routes/_coordinator/internship-coordinator/index.tsx
@@ -7,25 +7,46 @@ import {
 } from "@/components/ui/card.tsx";
 import { Button } from "@/components/ui/button.tsx";
 import { useQuery, useQueryClient } from "@tanstack/react-query";
+import { CircleUserRound } from "lucide-react";
 
 export const Route = createFileRoute("/_coordinator/internship-coordinator/")({
   component: RouteComponent,
 });
 
-function RouteComponent() {
+async function RouteComponent() {
   const queryClient = useQueryClient();
 
-  const students = useQuery({
-    queryKey: ["/api/coordinator/users/role=student"],
+  const studentsQuery = useQuery({
+    queryKey: ["/api/coordinator/users?role=student&per_page=5"],
   });
+
+  const students = await studentsQuery.data;
 
   return (
     <>
+      <h1>Coördinator dashboard</h1>
+
       <Card>
         <CardHeader>
           <h2>Studenten</h2>
         </CardHeader>
-        <CardContent></CardContent>
+        <CardContent className="flex justify-between">
+          {students != null && students?.data?.data?.length > 0 ? (
+            students.data.data.map((student) => (
+              <div className="text-center">
+                <div className="h-20 w-20 rounded-full bg-accent">
+                  <CircleUserRound
+                    strokeWidth={1.2}
+                    className="h-full w-full text-creme"
+                  />
+                </div>
+                <p>{student.first_name}</p>
+              </div>
+            ))
+          ) : (
+            <p>geen studenten</p>
+          )}
+        </CardContent>
         <CardFooter>
           <div className="w-full text-right">
             <Button asChild>
--- src/routes/_coordinator/internship-coordinator/students.tsx
+++ src/routes/_coordinator/internship-coordinator/students.tsx
@@ -1,11 +1,34 @@
-import { createFileRoute } from '@tanstack/react-router'
+import { createFileRoute } from "@tanstack/react-router";
+import { useQuery, useQueryClient } from "@tanstack/react-query";
+import { CircleUserRound } from "lucide-react";
+import StudentCard from "@/routes/_coordinator/internship-coordinator/-components/student-card.tsx";
 
 export const Route = createFileRoute(
-  '/_coordinator/internship-coordinator/students',
+  "/_coordinator/internship-coordinator/students",
 )({
   component: RouteComponent,
-})
+});
 
-function RouteComponent() {
-  return <div>Hello "/_coordinator/internship-coordinator/students"!</div>
+async function RouteComponent() {
+  const queryClient = useQueryClient();
+
+  const studentsQuery = useQuery({
+    queryKey: ["/api/coordinator/users?role=student&per_page=5"],
+  });
+
+  const students = await studentsQuery.data;
+
+  return (
+    <>
+      <h1>Studenten overzicht</h1>
+
+      <div className="flex flex-col gap-1">
+        {students != null && students?.data?.data?.length > 0 ? (
+          students.data.data.map((student) => <StudentCard student={student} />)
+        ) : (
+          <p>geen studenten</p>
+        )}
+      </div>
+    </>
+  );
 }
