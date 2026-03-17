3b4001a5b07ab22f466aca0556c334dd875740e3
Christa <christa_pol1@hotmail.com>
2026-03-17 15:17:33 +0100

    Loading state index added

--- src/routes/_student/vacancies/index.tsx
+++ src/routes/_student/vacancies/index.tsx
@@ -2,6 +2,8 @@ import { createFileRoute } from "@tanstack/react-router";
 import VacancyCard from "@/routes/_student/vacancies/-components/vacancy-card.tsx";
 import { useQuery } from "@tanstack/react-query";
 import { H1 } from "@/components/ui/headings.tsx";
+import { Card, CardContent } from "@/components/ui/card.tsx";
+import { Skeleton } from "@/components/ui/skeleton.tsx";
 
 export const Route = createFileRoute("/_student/vacancies/")({
   component: RouteComponent,
@@ -9,16 +11,40 @@ export const Route = createFileRoute("/_student/vacancies/")({
 
 async function RouteComponent() {
   // const [data, setData] = useState(null);
-  const { data, status, error } = useQuery({
+  const { data, isLoading } = useQuery({
     queryKey: ["api/vacancies"],
   });
 
-  if (status === "pending") {
-    return <span>Aan het laden...</span>;
-  }
-
-  if (status === "error") {
-    return <span>Error: {error.message}</span>;
+  if (isLoading) {
+    return (
+      <div>
+        <H1>Aanbevolen stageopdrachten</H1>
+        <Card>
+          <CardContent className="flex flex-col gap-10">
+            <Skeleton className="h-5 w-50" />
+            <div className="flex justify-between gap-6">
+              <div className="flex gap-6">
+                <div className="flex flex-col gap-2 text-center">
+                  <Skeleton className="h-25 w-25 rounded-full" />
+                  <Skeleton className="h-5 w-25" />
+                </div>
+                <div className="flex flex-col gap-2">
+                  <Skeleton className="h-5 w-100" />
+                  <div className="flex gap-2">
+                    <Skeleton className="h-5 w-15" />
+                    <Skeleton className="h-5 w-15" />
+                  </div>
+                </div>
+              </div>
+              <div className="flex flex-col gap-2 text-center">
+                <Skeleton className="h-5 w-30" />
+                <Skeleton className="h-30 w-30 rounded-full" />
+              </div>
+            </div>
+          </CardContent>
+        </Card>
+      </div>
+    );
   }
 
   return (
